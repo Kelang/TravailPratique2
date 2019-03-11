c3dfe19d80e1d3e19f270e9aa20c663e1faeba94
Kevin Langlois <k.langlois001@gmail.com>
2019-03-11 18:04:43 -0400

    Ajout des champs manquants aux articles

---
+++ template-parts/content/content-single.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( ! twentynineteen_can_show_post_thumbnail() ) : ?>
+	<header class="entry-header">
+		<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+	</header>
+	<?php endif; ?>
+
+	<div class="entry-content">
+		<?php
+		the_content(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			)
+		);
+
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'twentynineteen' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+		<?php if( get_field('nombres_dheures') ): ?>
+		<section class='champ'>
+			Nombre d'heures :
+			<?php the_field('nombres_dheures'); ?>
+		</section>
+		<?php endif; ?>
+		<?php if( get_field('professeurs') ): ?>
+		<section class='champ'>
+			Professeurs :
+			<?php the_field('professeurs'); ?>
+		</section>
+		<?php endif; ?>
+		<?php if( get_field('image1') ): ?>
+		
+		<img src="<?php the_field('image1'); ?>" />
+
+		<?php endif; ?>
+		<?php if( get_field('image2') ): ?>
+		
+		<img src="<?php the_field('image2'); ?>" />
+
+		<?php endif; ?>
+		<?php if( get_field('image3') ): ?>
+		
+		<img src="<?php the_field('image3'); ?>" />
+
+		<?php endif; ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php twentynineteen_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+	<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
+	<?php endif; ?>
+
+</article><!-- #post-${ID} -->
